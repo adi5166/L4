76b361eb02d5b8b6f19ad7aa40f3efefa2df5422
adi5166 <45405407+adi5166@users.noreply.github.com>
2020-04-17 14:53:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/l4/TaskFragment.java
@@ -0,0 +1,77 @@
+package com.example.l4;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.l4.tasks.TaskListContent;
+import com.example.l4.tasks.TaskListContent.Task;
+
+public class TaskFragment extends Fragment {
+
+    private MyTaskRecyclerViewAdapter mRecyclerViewAdapter;
+
+    private OnListFragmentInteractionListener mListener;
+
+    public TaskFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+            mRecyclerViewAdapter = new MyTaskRecyclerViewAdapter(TaskListContent.ITEMS, mListener);
+            recyclerView.setAdapter(mRecyclerViewAdapter);
+        }
+        return view;
+    }
+
+    public void notifyDataChange() {
+        mRecyclerViewAdapter.notifyDataSetChanged();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onListFragmentClickInteraction(Task task, int position);
+        void onListFragmentLongClickInteraction(int position);
+    }
+}
---
+++ app/src/main/java/com/example/l4/tasks/TaskListContent.java
@@ -0,0 +1,64 @@
+package com.example.l4.tasks;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TaskListContent {
+
+    public static final List<Task> ITEMS = new ArrayList<Task>();
+    public static final Map<String, Task> ITEM_MAP = new HashMap<String, Task>();
+
+    private static final int COUNT = 5;
+
+    static {
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+    }
+
+    public static void addItem(Task item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static Task createDummyItem(int position) {
+        return new Task(String.valueOf(position), "Item " + position, makeDetails(position));
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    public static class Task {
+        public final String id;
+        public final String title;
+        public final String details;
+        public final String picPath;
+
+        public Task(String id, String content, String details) {
+            this.id = id;
+            this.title = content;
+            this.details = details;
+            this.picPath = "";
+        }
+        public Task(String id, String content, String details, String picPath) {
+            this.id = id;
+            this.title = content;
+            this.details = details;
+            this.picPath = picPath;
+        }
+
+        @Override
+        public String toString() {
+            return title;
+        }
+    }
+}
